788127c8d4791b04e723610731940cce69761196
yuvalmaor <yuvalmaor305@gmail.com>
2022-01-26 22:12:55 +0200

    Create server.py
    few

---
+++ server.py
@@ -0,0 +1,17 @@
+import socket
+
+HOST = '0.0.0.0'  # Standard loopback interface address (localhost)
+PORT = 54444        # Port to listen on (non-privileged ports are > 1023)
+print("start")
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            print(data)
+            if not data:
+                break
+            conn.sendall(data)

a14246d9472fa3883d7aa1203ee28becad2e49ff
yuvalmaor <yuvalmaor305@gmail.com>
2022-01-26 23:09:18 +0200

    Update server.py
    sadsa

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 
 HOST = '0.0.0.0'  # Standard loopback interface address (localhost)
-PORT = 54444        # Port to listen on (non-privileged ports are > 1023)
+PORT = 2345        # Port to listen on (non-privileged ports are > 1023)
 print("start")
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
